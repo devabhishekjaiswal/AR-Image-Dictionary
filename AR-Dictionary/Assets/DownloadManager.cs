f20133778905ab238605a7797c846a67649676c0
Abhishek Jaiswal <abshk246@gmail.com>
2018-05-13 12:45:05 +0530

    adding source code and report

---
+++ AR-Dictionary/Assets/DownloadManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+ 
+public class DownloadManager : MonoBehaviour {
+    private WWW wwwData;
+    private static DownloadManager dm = null;
+   
+    // Use this for initialization
+    void Start () {
+        if( DownloadManager.dm == null )
+            DownloadManager.dm = FindObjectOfType(typeof(DownloadManager)) as DownloadManager;     
+    }
+   
+    // Update is called once per frame
+    void Update () {
+   
+    }
+   
+    void OnApplicationQuit()
+    {
+        DownloadManager.dm = null;
+    }
+   
+    public delegate void DownloadCallback(string data, string sError, string text);
+   
+    private IEnumerator WaitForDownload(DownloadCallback fn, string text)
+    {
+        Debug.Log("Yielding");     
+        yield return wwwData;
+        Debug.Log("Yielded");
+        fn(wwwData.text, wwwData.error, text);  
+    }
+   
+    private void StartDownload(string sURL, DownloadCallback fn, string text)
+    {
+        try
+        {  
+            wwwData = new WWW(sURL);           
+            Debug.Log("Starting download.");
+            //StartCoroutine("WaitForDownload", fn, text);
+            StartCoroutine(WaitForDownload(fn, text));
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.ToString());
+        }  
+    }
+   
+    public static void Download(string sURL, DownloadCallback fn, string text)
+    {
+        dm.StartDownload(sURL, fn, text);
+    }
+}
